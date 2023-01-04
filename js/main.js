edd0757f4def535415f8548d52e63dbe8dfb2bd5
GrahamZen <931252924@qq.com>
2023-01-04 01:06:57 -0500

    Site updated: 2023-01-04 01:06:57

--- js/main.js
+++ js/main.js
@@ -122,7 +122,6 @@
 
     const $toc = $('#toc');
     if ($toc.length > 0) {
-        $toc.addClass('column-left is-sticky');
         const $mask = $('<div>');
         $mask.attr('id', 'toc-mask');
 
